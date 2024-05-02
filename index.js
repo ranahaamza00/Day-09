8a02a84571fcc73b8ac139014119804a7f06241d
ranahaamza00 <ranahaamza00@gmail.com>
2024-05-02 17:17:40 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+//Qno : 25
+var alien_color = "green";
+if (alien_color = "green") {
+    console.log("you just earned 5 points!");
+}
+alien_color = "red";
+if (alien_color == "green") {
+    // No output because the condition is false
+}
+//Qno : 26 
+//It will Run If Condition 
+alien_color = "green";
+if (alien_color = "green") {
+    console.log("you earned 10 points to kill aliens");
+}
+else {
+    console.log("you just earned 5 points.");
+}
+// It wil run Else Condition
+alien_color = "yellow";
+if (alien_color == "green") {
+    console.log("You  earned 10 points to shooting aliens");
+}
+else {
+    console.log("you earned just earned 5 points");
+}
+//Qno : 27 
+alien_color = "green";
+if (alien_color == "green") {
+    console.log("You earned 5 points");
+}
+else if (alien_color == "yellow") {
+    console.log("You earned 10 points");
+}
+else if (alien_color == "red") {
+    console.log("you earned 15 points");
+}
+alien_color = "yellow";
+if (alien_color == "green") {
+    console.log("You earned 5 points");
+}
+else if (alien_color == "yellow") {
+    console.log("You earned 10 points");
+}
+else if (alien_color == "red") {
+    console.log("You earned 15 points");
+}
+alien_color = "red";
+if (alien_color == "green") {
+    console.log("You earned 5 points");
+}
+else if (alien_color == "yellow") {
+    console.log("You earned 10 points");
+}
+else if (alien_color == "red") {
+    console.log("You earned 15 points");
+}
